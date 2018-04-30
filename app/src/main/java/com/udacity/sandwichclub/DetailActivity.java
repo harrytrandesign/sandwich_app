30f13b4b296befa567bd4194cc427009c048688c
Harry Tran <harrytrandesign@gmail.com>
2018-04-29 22:50:30 -0700

    Implemented JSONObject inside JsonUtils.
    In DetailsActivity matched id's with views in setupLayout() and populated populateUI method with data parsed from the Sandwich object.
    
    Added some test strings for checking layout design.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,9 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,21 +17,29 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    ImageView ingredientsIv;
+    TextView tvOrigin;
+    TextView tvAlias;
+    TextView tvIngredients;
+    TextView tvDescription;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        setupLayout();
 
         Intent intent = getIntent();
         if (intent == null) {
+            Log.i("data", "Intent was null");
             closeOnError();
         }
 
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
+            Log.i("data", "Couldn't get a position");
             closeOnError();
             return;
         }
@@ -37,13 +47,21 @@ public class DetailActivity extends AppCompatActivity {
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
         Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+
+        Log.i("sandwich", sandwich.getMainName());
+        // Log the sandwich data
+        Log.i("data", json);
+        Log.i("data", "The position of the sandwich " + position);
+
         if (sandwich == null) {
             // Sandwich data unavailable
-            closeOnError();
+            Log.i("data", "There was no sandwich data");
+//            closeOnError();
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
+
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +74,51 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    // Match IDs to the view
+    private void setupLayout() {
+        ingredientsIv = findViewById(R.id.image_iv);
+        tvOrigin = findViewById(R.id.origin_tv);
+        tvAlias = findViewById(R.id.also_known_tv);
+        tvIngredients = findViewById(R.id.ingredients_tv);
+        tvDescription = findViewById(R.id.description_tv);
+    }
+
+    // Populate the layout with information from Sandwich Object
+    private void populateUI(Sandwich sandwich) {
+
+        // Use a StringBuilder to append a list together for aka and ingredients
+        StringBuilder aliasBuilder = new StringBuilder();
+        String alsoKnownAsList = "";
+        String ingredientsList = "";
+
+        int counterOne = 0; // So last iteration doesn't have a new line break;
+        for (String aka : sandwich.getAlsoKnownAs()) {
+            if (counterOne++ == sandwich.getAlsoKnownAs().size() - 1) {
+                aliasBuilder.append(aka);
+            } else {
+                aliasBuilder.append(aka).append("\n");
+            }
+        }
+
+        alsoKnownAsList = aliasBuilder.toString();
+
+        StringBuilder ingredientsBuilder = new StringBuilder();
+
+        int counterTwo = 0; // So last iteration doesn't have a new line break;
+        for (String ingredients : sandwich.getIngredients()) {
+            if (counterTwo++ == sandwich.getIngredients().size() - 1) {
+                ingredientsBuilder.append(ingredients);
+            } else {
+                ingredientsBuilder.append(ingredients).append("\n");
+            }
+        }
+
+        ingredientsList = ingredientsBuilder.toString();
+
+        tvOrigin.setText(sandwich.getPlaceOfOrigin());
+        tvAlias.setText(alsoKnownAsList);
+        tvIngredients.setText(ingredientsList);
+        tvDescription.setText(sandwich.getDescription());
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,73 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
+    private static final String NAME = "name";
+    private static final String MAIN_NAME = "mainName";
+    private static final String AKA = "alsoKnownAs";
+    private static final String ORIGIN = "placeOfOrigin";
+    private static final String DESCRIPTION = "description";
+    private static final String IMAGE = "image";
+    private static final String INGREDIENTS = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        Sandwich sandwich = null;
+        try {
+
+            JSONObject jsonObject = new JSONObject(json);
+
+            sandwich = new Sandwich(
+                    jsonObject.getJSONObject(NAME).getString(MAIN_NAME),
+                    getAliases(jsonObject),
+                    jsonObject.getString(ORIGIN),
+                    jsonObject.getString(DESCRIPTION),
+                    jsonObject.getString(IMAGE),
+                    getIngredients(jsonObject)
+            );
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return sandwich;
     }
+
+    // Convert a JSONArray into a List
+    private static List<String> convertArrayToList(JSONArray jsonArray) throws JSONException {
+
+        List<String> resultList = new ArrayList<>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            resultList.add(jsonArray.getString(i));
+        }
+
+        return resultList;
+    }
+
+    // Get a JSONArray and return it as a List for Aliases and Ingredients
+    private static List<String> getAliases(JSONObject jsonObjectAliases) throws JSONException {
+
+        JSONArray jsonArray = jsonObjectAliases.getJSONObject(NAME).getJSONArray(AKA);
+
+        return convertArrayToList(jsonArray);
+
+    }
+
+    private static List<String> getIngredients(JSONObject jsonObjectIngredients) throws JSONException {
+
+        JSONArray jsonArray = jsonObjectIngredients.getJSONArray(INGREDIENTS);
+
+        return convertArrayToList(jsonArray);
+
+    }
+
 }
