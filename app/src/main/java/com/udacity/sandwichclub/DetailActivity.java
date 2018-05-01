8420c8dfff3aaab766a8bedb34f4c82882dd2753
Harry Tran <harrytrandesign@gmail.com>
2018-04-30 18:00:24 -0700

    Cleaned up code that was commented out, implemented suggestions from code review.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,6 +3,7 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,34 +13,52 @@ import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DetailActivity extends AppCompatActivity {
 
+    // Set TAGS for Logging
+    public static final String LOG_TAG_DATA = "data";
+    public static final String LOG_TAG_SAND = "sandwich";
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
-    ImageView ingredientsIv;
-    TextView tvOrigin;
-    TextView tvAlias;
-    TextView tvIngredients;
-    TextView tvDescription;
+    @BindView(R.id.image_iv) ImageView ingredientsIv;
+    @BindView(R.id.origin_tv) TextView tvOrigin;
+    @BindView(R.id.also_known_tv) TextView tvAlias;
+    @BindView(R.id.ingredients_tv) TextView tvIngredients;
+    @BindView(R.id.description_tv) TextView tvDescription;
+
+    // Match IDs to the view
+    // No longer using this method, switching to Butterknife library
+    private void setupLayout() {
+        ingredientsIv = findViewById(R.id.image_iv);
+        tvOrigin = findViewById(R.id.origin_tv);
+        tvAlias = findViewById(R.id.also_known_tv);
+        tvIngredients = findViewById(R.id.ingredients_tv);
+        tvDescription = findViewById(R.id.description_tv);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        setupLayout();
+        // Testing out Butterknife bindview
+        ButterKnife.bind(this);
+//        setupLayout();
 
         Intent intent = getIntent();
         if (intent == null) {
-            Log.i("data", "Intent was null");
+            Log.i(LOG_TAG_DATA, "Intent was null");
             closeOnError();
         }
 
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
-            Log.i("data", "Couldn't get a position");
+            Log.i(LOG_TAG_DATA, "Couldn't get a position");
             closeOnError();
             return;
         }
@@ -48,22 +67,25 @@ public class DetailActivity extends AppCompatActivity {
         String json = sandwiches[position];
         Sandwich sandwich = JsonUtils.parseSandwichJson(json);
 
-        Log.i("sandwich", sandwich.getMainName());
+        Log.i(LOG_TAG_SAND, sandwich.getMainName());
         // Log the sandwich data
-        Log.i("data", json);
-        Log.i("data", "The position of the sandwich " + position);
+        Log.i(LOG_TAG_DATA, json);
+        Log.i(LOG_TAG_DATA, "The position of the sandwich " + position);
 
         if (sandwich == null) {
             // Sandwich data unavailable
-            Log.i("data", "There was no sandwich data");
-//            closeOnError();
+            Log.i(LOG_TAG_DATA, "There was no sandwich data");
+            closeOnError();
             return;
         }
 
         populateUI(sandwich);
 
+        // Used placeholder and error methods per suggestion from review
         Picasso.with(this)
                 .load(sandwich.getImage())
+                .placeholder(R.drawable.allblackhorizontal)
+                .error(R.drawable.allblackhorizontal)
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
@@ -74,51 +96,27 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    // Match IDs to the view
-    private void setupLayout() {
-        ingredientsIv = findViewById(R.id.image_iv);
-        tvOrigin = findViewById(R.id.origin_tv);
-        tvAlias = findViewById(R.id.also_known_tv);
-        tvIngredients = findViewById(R.id.ingredients_tv);
-        tvDescription = findViewById(R.id.description_tv);
-    }
-
     // Populate the layout with information from Sandwich Object
     private void populateUI(Sandwich sandwich) {
 
-        // Use a StringBuilder to append a list together for aka and ingredients
-        StringBuilder aliasBuilder = new StringBuilder();
-        String alsoKnownAsList = "";
-        String ingredientsList = "";
+        String alsoKnownAsList = TextUtils.join("\n", sandwich.getAlsoKnownAs());;
+        String ingredientsList = TextUtils.join("\n", sandwich.getIngredients());
 
-        int counterOne = 0; // So last iteration doesn't have a new line break;
-        for (String aka : sandwich.getAlsoKnownAs()) {
-            if (counterOne++ == sandwich.getAlsoKnownAs().size() - 1) {
-                aliasBuilder.append(aka);
-            } else {
-                aliasBuilder.append(aka).append("\n");
-            }
+        toggleViewVisibility(tvOrigin, sandwich.getPlaceOfOrigin());
+        toggleViewVisibility(tvAlias, alsoKnownAsList);
+        toggleViewVisibility(tvIngredients, ingredientsList);
+        toggleViewVisibility(tvDescription, sandwich.getDescription());
+
+    }
+
+    // Helper function to populate the textviews
+    public void toggleViewVisibility(TextView textView, String text) {
+
+        if (text.length() < 1) {
+            textView.setText(this.getResources().getString(R.string.unknown_details_label));
+        } else {
+            textView.setText(text);
         }
-
-        alsoKnownAsList = aliasBuilder.toString();
-
-        StringBuilder ingredientsBuilder = new StringBuilder();
-
-        int counterTwo = 0; // So last iteration doesn't have a new line break;
-        for (String ingredients : sandwich.getIngredients()) {
-            if (counterTwo++ == sandwich.getIngredients().size() - 1) {
-                ingredientsBuilder.append(ingredients);
-            } else {
-                ingredientsBuilder.append(ingredients).append("\n");
-            }
-        }
-
-        ingredientsList = ingredientsBuilder.toString();
-
-        tvOrigin.setText(sandwich.getPlaceOfOrigin());
-        tvAlias.setText(alsoKnownAsList);
-        tvIngredients.setText(ingredientsList);
-        tvDescription.setText(sandwich.getDescription());
 
     }
 }
